a5422618bc93c0a79b3c4e90bd5b15c438817035
warren yun <wyun2006@gmail.com>
2022-03-26 11:27:35 -0400

    init app

---
+++ frontend/frontend/Screens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  frontend
+//
+//  Created by warren yun on 3/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,24 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by warren yun on 3/26/22.
+//
+
+import SwiftUI
+import UIKit
+
+struct ContentView: View {
+    TabView {
+        
+    }
+}
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
